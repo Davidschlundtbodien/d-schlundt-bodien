3166d4314f94f8190e3bdca8d977a1c2ea937f3d
davidschlundtbodien <d.schlundtbodien@gmail.com>
2021-03-09 15:38:20 -0700

    adds navbar and footer components

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <h1>David Schlundt-Bodien</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
