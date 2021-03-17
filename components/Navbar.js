9f9a790c4ded600d1d3abf31e10198649fcc1627
davidschlundtbodien <d.schlundtbodien@gmail.com>
2021-03-17 11:13:37 -0600

    adds styling to navbar and footer components

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,13 +2,13 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav>      
+      <div className="logo">
         <h1>David Schlundt-Bodien</h1>
-        <Link href="/"><a>Home</a></Link>
-        <Link href="/about"><a>About</a></Link>
-        <Link href="/projects"><a>Projects</a></Link>
       </div>
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/about"><a>About</a></Link>
+      <Link href="/projects"><a>Projects</a></Link>
     </nav>
   );
 };
